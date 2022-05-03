fe341910edf3acf885e77f4079fe24e708610d5e
Nicolas Moron <nicolasmoron15@gmail.com>
2022-05-03 15:04:01 -0300

    Update

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -50,7 +50,7 @@ class PropiedadController
             //Realiza un resize a la imgen con intervention
             
             if ($_FILES['propiedad']['tmp_name']['imagen']) {
-                debuguear(Image::getManager());
+                debuguear(Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600));
                 $image = Image::make($_FILES['propiedad']['tmp_name']['imagen'])->fit(800, 600);
                 $propiedad->setImagen($nombreImagen);
             }
