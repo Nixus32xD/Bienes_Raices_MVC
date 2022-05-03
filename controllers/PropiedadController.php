20174cfd5631ea511441582908413fc33e0c501a
Nicolas Moron <nicolasmoron15@gmail.com>
2022-05-03 12:18:15 -0300

    Update

--- controllers/PropiedadController.php
+++ controllers/PropiedadController.php
@@ -40,7 +40,7 @@ class PropiedadController
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             //Crea una nueva instancia
-            debuguear($_FILES['propiedad']['tmp_name']);
+            debuguear($_FILES);
             $propiedad = new Propiedad($_POST['propiedad']);
 
             //Subida de archivos
